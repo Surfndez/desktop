9bd28854bb06ea75b92fc0b7a5684d163698990f
Brendan Forster <github@brendanforster.com>
2019-02-05 15:12:16 -0400

    refine the rebase flow so that it will return to show conflicts if the rebase is aborted

--- app/src/lib/stores/app-store.ts
+++ app/src/lib/stores/app-store.ts
@@ -3282,7 +3282,7 @@ export class AppStore extends TypedBaseStore<IAppState> {
       return f.status.kind !== AppFileStatusKind.Untracked
     })
 
-    await gitStore.performFailableOperation(() =>
+    return await gitStore.performFailableOperation(() =>
       continueRebase(repository, trackedFiles)
     )
   }
